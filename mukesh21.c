879cb5cb8d826fd4b35c5130a9d3565aa5f58d18
manimukesh21 <35652371+manimukesh21@users.noreply.github.com>
2018-01-26 15:54:23 +0530

    Create mukesh21.c

---
+++ mukesh21.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+    char i;
+   printf("Enter\n");
+   scanf("%c",&i);
+     if((i>='a' && i<='z')||(i>='A' && i<='Z'))
+     printf("it is an alphabet\n");
+     else
+     printf("it is an not alphabet");
+     
+}
