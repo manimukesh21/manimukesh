913ad79adabcbf511f8b4d529b0c7cdf2dcb7ff5
manimukesh21 <35652371+manimukesh21@users.noreply.github.com>
2018-03-01 16:58:40 +0530

    Create mukeshnew10.c

---
+++ mukeshnew10.c
@@ -0,0 +1,16 @@
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b,i;
+printf("enter number:");
+scanf("%d",&a);
+printf("enter range");
+scanf("%d",&b);
+for(i=0;i<=b;++i)
+{
+printf("%d * %d =%d\n",a,i,a*i);
+getch();
+return 0;
+}
