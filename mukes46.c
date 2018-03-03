17ec796d22d8ec15ba4c0f6d6337b1ca46a253d0
manimukesh21 <35652371+manimukesh21@users.noreply.github.com>
2018-03-03 15:52:31 +0530

    Create mukes46.c

---
+++ mukes46.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(void) {
+	int n,i,temp,j;
+	scanf("%d",&n);
+	int a[n];
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		for(j=i+1;j<n;j++)
+		{
+			if(a[j]<a[i])
+			{
+				temp=a[i];
+				a[i]=a[j];
+				a[j]=temp;
+			}
+			
+		}
+	
+	}
+  i=n/2;
+  printf("%d",a[i]);
+	
+	return 0;
+}
