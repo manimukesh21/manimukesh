f041a90c1ca6fa799bcb622a81662fc35fbd6e49
manimukesh21 <35652371+manimukesh21@users.noreply.github.com>
2018-03-03 16:07:37 +0530

    Update and rename mukeshnew1.c to mukeshnew551.c

---
+++ mukeshnew551.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+int s,t,m,n,i;
+t=0;
+m=1;
+printf("Enter the number%d\n%d",t,m);
+scanf("%d,%d",&t,&m);
+for(i=2;i<n;i++)
+{
+s=t+m;
+t=m;
+m=s;
+}
+printf("calculation %d",s);
+}
