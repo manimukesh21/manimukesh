abe21b408ac6892576cde780b3531c73a4a72591
manimukesh21 <35652371+manimukesh21@users.noreply.github.com>
2018-03-03 16:19:26 +0530

    Create mukeshnew5527.c

---
+++ mukeshnew5527.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	char num;
+	printf("Enter the Input:");
+	scanf("%c",&num);
+	if(num>=0&&num<=9)
+	{
+		printf("Yes");
+	}
+	else
+	{
+		printf("No");
+	}
+}
+getch();
+}
