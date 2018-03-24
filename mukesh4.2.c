ccde26f2ad3ac6a700ef12fe274db56517efecff
manimukesh21 <35652371+manimukesh21@users.noreply.github.com>
2018-03-24 13:13:30 +0530

    Create mukesh4.2.c

---
+++ mukesh4.2.c
@@ -0,0 +1,20 @@
+ 
+#include <stdio.h>
+#include<conio.h>	   
+#include<string.h>	   
+int main() 	   
+{	   
+char a[100];	   
+int n,i,count=1;	   
+scanf("%[^\t\n]s",a);	   
+n=strlen(a);	   
+for(i=0;i<n;i++)	   
+{	   
+if(a[i]=='')	   
+{	   
+count++;	   
+}	   
+}	   
+printf("%d",count);	   
+return 0;	 
+}
