18062bda0341504766c18e9f088c07e56f1e3959
manimukesh21 <35652371+manimukesh21@users.noreply.github.com>
2018-03-24 13:14:19 +0530

    Create mukesh4.3.c

---
+++ mukesh4.3.c
@@ -0,0 +1,17 @@
+ 
+#include<stdio.h>
+#include<conio.h>	   
+#include<string.h>	   
+int main()	   
+{	   
+char str[50];	   
+int c=0,i;	   
+scanf("%[^\n]s",str);	   
+int l;	   
+l=strlen(str);	   
+for (i = 0; i<l; i++)	   
+if (str[i] == '')	   
+c++;	   
+printf("%d",c);	   
+return 0;	 
+}
