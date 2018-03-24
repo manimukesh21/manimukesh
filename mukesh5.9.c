a60f6d3d0d7638d89948cb22c639c01c96fd8451
manimukesh21 <35652371+manimukesh21@users.noreply.github.com>
2018-03-24 13:28:30 +0530

    Create mukesh5.9.c

---
+++ mukesh5.9.c
@@ -0,0 +1,12 @@
+ 
+#include<stdio.h>
+#include<conio.h>	   
+int main()	   
+{	   
+int n1,n2,sum=0;	   
+scanf("%d",&n1);	   
+scanf("%d",&n2);	   
+sum=n1+n2;	   
+printf("%d",sum);	   
+return 0;	 
+}
