52228eeaf500c0f718026f1f61a28e71a031ab18
manimukesh21 <35652371+manimukesh21@users.noreply.github.com>
2018-03-24 13:27:15 +0530

    Create mukesh5.7.c

---
+++ mukesh5.7.c
@@ -0,0 +1,17 @@
+ 
+#include<stdio.h>
+#include<conio.h>	   
+void main()	   
+{	   
+int a=10;	   
+int b=20;	   
+int c=30;	   
+if(a<b)	   
+{	   
+printf("%d",a);	   
+}	   
+if(b<c)	   
+{	   
+printf("%d",c);	   
+}	 
+}
