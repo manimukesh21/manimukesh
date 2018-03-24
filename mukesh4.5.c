283c7a483aca3847ed307ef20362ae03ee39495a
manimukesh21 <35652371+manimukesh21@users.noreply.github.com>
2018-03-24 13:16:15 +0530

    Create mukesh4.5.c

---
+++ mukesh4.5.c
@@ -0,0 +1,21 @@
+ 
+#include <stdio.h>
+#include<conio.h>	   
+#include<string.h>	   
+int main(void) 
+{	   
+char ch[100];	   
+int i,k,count=0;	   
+gets(ch);	   
+for(k=0;ch[k]!='\0';++k)	   
+{	   
+	   
+}	   
+for(i=0;i<k;i++)	   
+{	   
+if(isdigit(ch[i]))	   
+count++;	   
+}	   
+printf("%d",count);	   
+return 0;	   
+}	 
