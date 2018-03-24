21ae176b95ae7d41e89222d26b14e7bf577a37ec
manimukesh21 <35652371+manimukesh21@users.noreply.github.com>
2018-03-24 13:22:23 +0530

    Create mukesh5.1.c

---
+++ mukesh5.1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+	char a[100];
+	int i,n;
+	scanf("%s",a);
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+	    printf("%s\n",a);
+	 }
+	 return 0;
+}
